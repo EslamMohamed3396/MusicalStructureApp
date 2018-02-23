9ab817700ab65b2264180b3040d77e442a623118
Eslam <em0109224@gmail.com>
2018-02-24 00:20:40 +0200

    MusicalStructure

--- app/src/main/java/com/example/eslam/musicalstructureapp/AllSong.java
+++ app/src/main/java/com/example/eslam/musicalstructureapp/AllSong.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RadioButton;
 
 import java.util.ArrayList;
 
@@ -16,10 +17,10 @@ public class AllSong extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_of_song);
         ArrayList<SongDetails> song = new ArrayList<SongDetails>();
-        song.add(new SongDetails("The Greatest", "Sia", "3:31"));
-        song.add(new SongDetails("Faded", "Alan Walker", "3:32"));
-        song.add(new SongDetails("Alive", "Sia", "4:23"));
-        song.add(new SongDetails("Bird Set Free", "Sia", "4:12"));
+        song.add(new SongDetails(getResources().getString(R.string.TheGreatest),getResources().getString(R.string.Sia) , "3:31"));
+        song.add(new SongDetails(getResources().getString(R.string.Faded), getResources().getString(R.string.AlanWalker), "3:32"));
+        song.add(new SongDetails(getResources().getString(R.string.Alive), getResources().getString(R.string.Sia), "4:23"));
+        song.add(new SongDetails(getResources().getString(R.string.Bird), getResources().getString(R.string.Sia), "4:12"));
         Adabter adabter = new Adabter(this, song);
         ListView list = findViewById(R.id.list_song);
         list.setAdapter(adabter);
